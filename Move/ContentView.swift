8b8bc299b4d0a171b61cb29b40f1bb7df5272073
Sakarn Limnitikarn <bankiiee@gmail.com>
2020-09-03 10:54:59 +0700

    Initial Commit

---
+++ Move/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Move
+//
+//  Created by bankiiee on 3/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Move/MoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveApp.swift
+//  Move
+//
+//  Created by bankiiee on 3/9/20.
+//
+
+import SwiftUI
+
+@main
+struct MoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
